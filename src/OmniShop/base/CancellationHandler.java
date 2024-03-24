8e90b47dc814e34c275416cae1ef57e7463433a8
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2024-03-24 21:26:20 +0530

    Starting new project with new target

--- src/OmniShop/base/CancellationHandler.java
+++
@@ -1,7 +0,0 @@
-public class CancellationHandler {
-    public void cancelOrder(Order order) {
-        // Implement order cancellation logic
-        // Update order status, refund payment if applicable, etc.
-    }
-}
-
